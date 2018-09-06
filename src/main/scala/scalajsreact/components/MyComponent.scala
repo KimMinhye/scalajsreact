cdeba4e0b5ccd0dac70b3ad4df984b25d0992577
Kim Minhye <mine517@nate.com>
2018-09-06 11:24:41 +0900

    state example

--- src/main/scala/scalajsreact/components/MyComponent.scala
+++ src/main/scala/scalajsreact/components/MyComponent.scala
@@ -4,9 +4,28 @@ import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 
 object MyComponent {
+  case class Props(name: String = "React", age: Int)
+  case class State(count: Int)
 
-val NoArgs =
-  ScalaComponent.builder[Unit]("No args")
-    .renderStatic(<.div("Hello!"))
+  class Backend($ : BackendScope[Props, State]) {
+    def onClick() = $.modState(s => State(s.count + 1))
+    def render(props: Props, state: State) = {
+      <.div(<.p(s"안녕하세요. 제 이름은 ${props.name}입니다."),
+            <.p(s"저는 ${props.age}입니다."),
+            <.p(s"숫자 : ${state.count}"),
+            <.button(^.onClick --> onClick, "더하기"))
+    }
+  }
+
+  val NoArgs =
+    ScalaComponent
+      .builder[Unit]("No args")
+      .renderStatic(<.div("Hello!"))
+      .build
+
+  val Component = ScalaComponent
+    .builder[Props]("Hello")
+    .initialState(State(0))
+    .renderBackend[Backend]
     .build
 }
